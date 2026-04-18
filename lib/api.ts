ec25f72e784ceec40d44de7e91a186f9d192db78
Bala-Saravanan <jbalasaravanan174@gmail.com>
2026-04-19 00:26:06 +0530

    Undo: last commit of today

--- lib/api.ts
+++ lib/api.ts
@@ -12,14 +12,14 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     // console.log(error);
-    // if (error.response?.status === 401) {
-    //   if (
-    //     typeof window !== "undefined" &&
-    //     window.location.pathname !== "/login"
-    //   ) {
-    //     window.location.href = "/login";
-    //   }
-    // }
+    if (error.response?.status === 401) {
+      if (
+        typeof window !== "undefined" &&
+        window.location.pathname !== "/login"
+      ) {
+        window.location.href = "/login";
+      }
+    }
     return Promise.reject(error);
   },
 );
